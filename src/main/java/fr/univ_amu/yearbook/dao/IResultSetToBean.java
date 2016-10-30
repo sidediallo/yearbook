f6c54aa793c309639b4602d3cea2de4a659baef1
inoussa <zongoinoussa01@gmail.com>
2016-10-30 10:18:36 +0100

    Creation of IResultSetToBean interface

---
+++ src/main/java/fr/univ_amu/yearbook/dao/IResultSetToBean.java
@@ -0,0 +1,22 @@
+package fr.univ_amu.yearbook.dao;
+
+import java.sql.ResultSet;
+
+import fr.univ_amu.yearbook.dao.exception.DAOException;
+
+/**
+ * The IResultsetToBean interfaces defines method to convert a result to a java bean.
+ * 
+ * @author Utilisateur
+ *@version 1.0
+ */
+public interface IResultSetToBean<T>{
+	/**
+	 * Method that convert a resultSet to a bean.
+	 * 
+	 * @param rs ResultSet to convert to bean.
+	 * @return the bean result of the conversion of the resultSet.
+	 * @throws DAOException
+	 */
+	public T toBean(ResultSet rs) throws DAOException;
+}
