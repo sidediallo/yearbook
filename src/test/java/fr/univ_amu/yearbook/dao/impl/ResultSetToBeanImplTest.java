c171abbe212cd5f7984d9b81a601f8cbc0671828
inoussa <zongoinoussa01@gmail.com>
2016-10-30 10:22:09 +0100

    Creation of ResultSetToBeanImpl Unit test

---
+++ src/test/java/fr/univ_amu/yearbook/dao/impl/ResultSetToBeanImplTest.java
@@ -0,0 +1,78 @@
+package fr.univ_amu.yearbook.dao.impl;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import fr.univ_amu.yearbook.bean.Group;
+import fr.univ_amu.yearbook.dao.exception.ConnectionManagerException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring.xml"})
+public class ResultSetToBeanImplTest {
+	@Autowired
+	DatabaseManagerImpl connManager;
+	ResultSetToBeanImpl<Group> rsTobean;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testToBean() throws ConnectionManagerException, SQLException {
+		Connection conn = connManager.newConnection();
+		PreparedStatement pstm = conn.prepareStatement("INSERT INTO groupe values(?,?)",Statement.RETURN_GENERATED_KEYS);
+		pstm.setObject(1, null);
+		Timestamp timestamp =  new Timestamp(System.currentTimeMillis());
+		String groupName = "Group "+timestamp.toString();
+		pstm.setString(2, groupName);
+		pstm.executeUpdate();
+		ResultSet pkrs = pstm.getGeneratedKeys();
+		
+		pkrs.next();
+		Integer groupId = pkrs.getInt(1);
+		
+		PreparedStatement pstm2 = conn.prepareStatement("SELECT * FROM groupe WHERE id = ?");
+		pstm2.setInt(1, groupId);
+		
+		ResultSet rs = pstm2.executeQuery();
+		
+		rsTobean = new ResultSetToBeanImpl<>(Group.class);
+		rs.next();
+		Group group = rsTobean.toBean(rs);
+		
+		
+		assertTrue(groupId == group.getId());
+		assertTrue(groupName.equals(group.getName()));
+		
+		connManager.closeConneection(conn);
+	}
+
+}
