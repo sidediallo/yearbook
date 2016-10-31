1b3fb0476676b18737cc7c6e569d8a035c008d02
sidediallo <sidediallo@yahoo.fr>
2016-10-31 13:54:03 +0100

    Implémentation de IPersonManager

--- src/main/java/fr/univ_amu/yearbook/bus/personManager/impl/PersonManager.java
+++ src/main/java/fr/univ_amu/yearbook/bus/personManager/impl/PersonManager.java
@@ -5,42 +5,96 @@ import java.util.Collection;
 import fr.univ_amu.yearbook.bean.Person;
 import fr.univ_amu.yearbook.bus.personManager.IPersonManager;
 import fr.univ_amu.yearbook.bus.personManager.exception.PersonManagerException;
+import fr.univ_amu.yearbook.dao.exception.DatabaseManagerException;
+import fr.univ_amu.yearbook.dao.exception.PersonDaoException;
+import fr.univ_amu.yearbook.dao.impl.PersonDao;
 
+/**
+ * <b>PersonManager</b> implémente IPersonManager.
+ *
+ * @see Person
+ * @see PersonManagerException
+ * @see IPersonManager
+ *  
+ * @author Aboubacar Sidy DIALLO & Inoussa ZONGO
+ * @version 1.0
+ *
+ */
 public class PersonManager implements IPersonManager {
+	private PersonDao dao = new PersonDao();
 
-	@Override
+	/**
+	 * Recherche et renvoie la personne associée à l'identifiant.
+	 * 
+	 * @param id L'id de la personne.
+	 * @return
+	 * 		La personne dont l'indentifiant est rentré en paramètre de la méthode. 
+	 * @throws PersonManagerException Si la personne rattachée à l'id n'existe pas.
+	 */
 	public Person findPerson(long id) throws PersonManagerException {
-		// TODO Auto-generated method stub
+		try {
+			return dao.findPerson(id);
+		} catch (PersonDaoException | DatabaseManagerException e) {
+			e.getMessage();
+		}
 		return null;
 	}
 
-	@Override
+	/**
+	 * Retourne la liste des personnes existantes.
+	 * 
+	 * @return
+	 * 		La liste de personnes.
+	 */
 	public Collection<Person> findAllPersons() {
-		// TODO Auto-generated method stub
+		try {
+			return dao.findAllPersons();
+		} catch (PersonDaoException | DatabaseManagerException e) {
+			e.getMessage();
+		}
 		return null;
 	}
 
-	@Override
+	/**
+	 * Création ou mise à jour d'une personne.
+	 * 
+	 * @param p La personne.
+	 * @throws PersonManagerException Si la personne qu'on souhaite rajouter existe déjà
+	 * 		   ou si la personne qu'on souhaite mettre à jour n'existe pas.
+	 * @see Person
+	 */
 	public void saveOrUpdatePerson(Person p) throws PersonManagerException {
-		// TODO Auto-generated method stub
-
+		try {
+			dao.saveOrUpdatePerson(p);
+		} catch (PersonDaoException e) {
+			e.getMessage();
+		}
 	}
 
-	@Override
+	/**
+	 * Suppression de la personne associé à l'identifiant id.
+	 * 
+	 * @param id L'id correspondant à la personne.
+	 * @throws PersonManagerException Si la personne rattachée à l'id n'existe pas.
+	 */
 	public void removePerson(long id) throws PersonManagerException {
-		// TODO Auto-generated method stub
-
+		dao.removePerson(id);
 	}
 
-	@Override
+	/**
+	 * Suppression de la personne.
+	 * 
+	 * @param p La personne à supprimer
+	 * @throws PersonManagerException Si la personne n'existe pas.
+	 */
 	public void removePerson(Person p) throws PersonManagerException {
-		// TODO Auto-generated method stub
-
+		dao.removePerson(p);
 	}
 
-	@Override
+	/**
+	 * Suppression de toute les personnes de la base.
+	 */
 	public void removeAllPersons() {
-		// TODO Auto-generated method stub
-
+		dao.removeAllPersons();
 	}
 }
