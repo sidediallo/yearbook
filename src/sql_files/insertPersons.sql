b9f27d46b5c6ec4fb7c7ba6ea3befa53b279b797
sidediallo <sidediallo@yahoo.fr>
2016-10-31 15:27:45 +0100

    Màj des données suite à la midification du fichier createTables.sql

--- src/sql_files/insertPersons.sql
+++ src/sql_files/insertPersons.sql
@@ -1,5 +1,5 @@
 -- Données de test insertion de personnes dans la base
 
-INSERT INTO YEARBOOK_Person_Person (lastName, firstName, email, homepage, birthDate, pwd, idG) VALUES ('DIALLO', 'Aboubacar Sidy', 'sidediallo@yahoo.fr', 'www.diallo_aboubacar.fr', STR_TO_DATE('2016/01/01','%Y/%m/%d'), PASSWORD('aboubacar'), 1);
-INSERT INTO YEARBOOK_Person_Person (lastName, firstName, email, homepage, birthDate, pwd, idG) VALUES ('KONDI', 'Marie', 'kondimarie@gmail.com', 'www.kondi_marie.fr', STR_TO_DATE('2016/01/01','%Y/%m/%d'), PASSWORD('marie'), 1);
-INSERT INTO YEARBOOK_Person_Person (lastName, firstName, email, homepage, birthDate, pwd, idG) VALUES ('CAMARA', 'Moussa', 'camaramoussa@gmail.com', 'www.camara_moussa.fr', STR_TO_DATE('2016/01/01','%Y/%m/%d'), PASSWORD('moussa'), 1);
+INSERT INTO YEARBOOK_Person (lastName, firstName, email, homepage, birthDate, pwd, idG) VALUES ('DIALLO', 'Aboubacar Sidy', 'sidediallo@yahoo.fr', 'www.diallo_aboubacar.fr', STR_TO_DATE('2016/01/01','%Y/%m/%d'), PASSWORD('aboubacar'), 1);
+INSERT INTO YEARBOOK_Person (lastName, firstName, email, homepage, birthDate, pwd, idG) VALUES ('KONDI', 'Marie', 'kondimarie@gmail.com', 'www.kondi_marie.fr', STR_TO_DATE('2016/01/01','%Y/%m/%d'), PASSWORD('marie'), 1);
+INSERT INTO YEARBOOK_Person (lastName, firstName, email, homepage, birthDate, pwd, idG) VALUES ('CAMARA', 'Moussa', 'camaramoussa@gmail.com', 'www.camara_moussa.fr', STR_TO_DATE('2016/01/01','%Y/%m/%d'), PASSWORD('moussa'), 1);
