2522152c84d49aed419d2a549dec26b883ff3d17
sidediallo <sidediallo@yahoo.fr>
2016-10-31 13:56:35 +0100

    Implémentation de l'interface IBeanToResultSet

---
+++ src/main/java/fr/univ_amu/yearbook/dao/impl/BeanToResultSet.java
@@ -0,0 +1,31 @@
+package fr.univ_amu.yearbook.dao.impl;
+
+import java.sql.ResultSet;
+
+import fr.univ_amu.yearbook.dao.IBeanToResultSet;
+import fr.univ_amu.yearbook.dao.exception.DAOException;
+
+/**
+ * <b>BeanToResultSet</b> implémente l'interface
+ * {@link IBeanToResultSet#toResultSet(Object, String, String[])}
+ *
+ * @see DAOException
+ *  
+ * @author Aboubacar Sidy DIALLO & Inoussa ZONGO
+ * @version 1.0
+ *
+ */
+public class BeanToResultSet<T> implements IBeanToResultSet<T> {
+	
+	/**
+	 * 
+	 * @param bean Le bean.
+	 * @param query La requête.
+	 * @param parametersList La liste des paramètres.
+	 * @return Le ResultSet correspondant ou null.
+	 * @throws DAOException Si une exception est levée.
+	 */
+	public ResultSet toResultSet(T bean, String query, String[] parametersList) throws DAOException {
+		return null;
+	}
+}
