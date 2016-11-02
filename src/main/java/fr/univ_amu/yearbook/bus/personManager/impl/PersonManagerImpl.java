470b5dae3d13917bcc03296cdec3d48748f997ac
sidediallo <sidediallo@yahoo.fr>
2016-11-02 16:43:37 +0100

    Ajout des getters, setters, constructeur vide et javadoc

--- src/main/java/fr/univ_amu/yearbook/bus/personManager/impl/PersonManagerImpl.java
+++ src/main/java/fr/univ_amu/yearbook/bus/personManager/impl/PersonManagerImpl.java
@@ -23,9 +23,16 @@ import fr.univ_amu.yearbook.dao.impl.PersonDaoImpl;
 public class PersonManagerImpl implements IPersonManager {
 	
 	/**
-	 * L'objet qui établi le lien avec la Dao.
+	 * L'objet qui établi le lien avec la PersonDao.
 	 * 
-	 * @see 
+	 * @see PersonManagerImpl#PersonManagerImpl()
+	 * @see PersonManagerImpl#init()
+	 * @see PersonManagerImpl#findPerson(long)
+	 * @see PersonManagerImpl#findAllPersons()
+	 * @see PersonManagerImpl#saveOrUpdatePerson(Person)
+	 * @see PersonManagerImpl#removePerson(long)
+	 * @see PersonManagerImpl#removePerson(Person)
+	 * @see PersonManagerImpl#removeAllPersons()
 	 */
 	private PersonDaoImpl dao;
 	
@@ -33,7 +40,17 @@ public class PersonManagerImpl implements IPersonManager {
 	 * Constructeur par défaut de la classe.
 	 */
 	public PersonManagerImpl() {
+		super();
+	}
+	
+	/**
+	 * Initialisation de la PersonDao
+	 * 
+	 * @throws DatabaseManagerException Si une exception est levée.
+	 */
+	public void init() throws DatabaseManagerException {
 		dao = new PersonDaoImpl();
+		dao.init();
 	}
 
 	/**
@@ -44,6 +61,7 @@ public class PersonManagerImpl implements IPersonManager {
 	 * 		La personne dont l'indentifiant est rentré en paramètre de la méthode. 
 	 * @throws PersonManagerException Si la personne rattachée à l'id n'existe pas.
 	 */
+	@Override
 	public Person findPerson(long id) throws PersonManagerException {
 		try {
 			return dao.findPerson(id);
@@ -59,6 +77,7 @@ public class PersonManagerImpl implements IPersonManager {
 	 * @return
 	 * 		La liste de personnes.
 	 */
+	@Override
 	public Collection<Person> findAllPersons() {
 		try {
 			return dao.findAllPersons();
@@ -76,6 +95,7 @@ public class PersonManagerImpl implements IPersonManager {
 	 * 		   ou si la personne qu'on souhaite mettre à jour n'existe pas.
 	 * @see Person
 	 */
+	@Override
 	public void saveOrUpdatePerson(Person p) throws PersonManagerException {
 		try {
 			dao.saveOrUpdatePerson(p);
@@ -90,6 +110,7 @@ public class PersonManagerImpl implements IPersonManager {
 	 * @param id L'id correspondant à la personne.
 	 * @throws PersonManagerException Si la personne rattachée à l'id n'existe pas.
 	 */
+	@Override
 	public void removePerson(long id) throws PersonManagerException {
 		dao.removePerson(id);
 	}
@@ -100,6 +121,7 @@ public class PersonManagerImpl implements IPersonManager {
 	 * @param p La personne à supprimer
 	 * @throws PersonManagerException Si la personne n'existe pas.
 	 */
+	@Override
 	public void removePerson(Person p) throws PersonManagerException {
 		dao.removePerson(p);
 	}
@@ -107,7 +129,26 @@ public class PersonManagerImpl implements IPersonManager {
 	/**
 	 * Suppression de toute les personnes de la base.
 	 */
+	@Override
 	public void removeAllPersons() {
 		dao.removeAllPersons();
+	}
+
+	/**
+	 * Retourne la PersonDao.
+	 * 
+	 * @return la PersonDao.
+	 */
+	public PersonDaoImpl getDao() {
+		return dao;
+	}
+
+	/**
+	 * Mise à jour de PersonDaoImpl.
+	 * 
+	 * @param dao La nouvelle PersonDao.
+	 */
+	public void setDao(PersonDaoImpl dao) {
+		this.dao = dao;
 	}
 }
