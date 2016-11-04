8345f0db3a49e0af0e1c486e143a4050a8780a88
sidediallo <sidediallo@yahoo.fr>
2016-11-05 00:51:05 +0100

    Ajout de DAOException et des commentaires javadoc

--- src/main/java/fr/univ_amu/yearbook/dao/IPersonDao.java
+++ src/main/java/fr/univ_amu/yearbook/dao/IPersonDao.java
@@ -3,7 +3,6 @@ package fr.univ_amu.yearbook.dao;
 import java.util.Collection;
 
 import fr.univ_amu.yearbook.bean.Person;
-import fr.univ_amu.yearbook.dao.exception.DatabaseManagerException;
 import fr.univ_amu.yearbook.dao.exception.DAOException;
 
 /**
@@ -24,59 +23,55 @@ public interface IPersonDao {
 	 * 
 	 * @param id L'id de la personne.
 	 * @return
-	 * 		La personne dont l'indentifiant est rentré en paramètre de la méthode. 
-	 * @throws DAOException Si la personne rattachée à l'id n'existe pas.
-	 * @throws JdbcToolsException Si la connection n'est pas établie.
+	 * 		La personne dont l'indentifiant est rentré en paramètre de la méthode ou null. 
+	 * @throws DAOException Si la personne rattachée à l'id n'existe pas ou si la connexion échoue.
 	 */
-	public Person findPerson(long id) throws DAOException, DatabaseManagerException;
+	public Person findPerson(long id) throws DAOException;
 	
 	/**
 	 * Retourne la liste des personnes existantes.
 	 * 
 	 * @return
 	 * 		La liste de personnes.
-	 * @throws DAOException S'il n'y a aucune personne.
-	 * @throws JdbcToolsException Si la connection n'est pas établie.
+	 * @throws DAOException S'il n'y a aucune personne dans la base ou si la connexion échoue.
 	 */
-	public Collection<Person> findAllPersons() throws DAOException, DatabaseManagerException;
+	public Collection<Person> findAllPersons() throws DAOException;
 	
 	/**
-	 * Création ou mise à jour d'une personne.
+	 * Création ou mise à jour des données d'une personne.
 	 * 
 	 * @param p La personne.
-	 * @throws DAOException Si la personne qu'on souhaite rajouter existe déjà
-	 * 		   ou si la personne qu'on souhaite mettre à jour n'existe pas.
 	 * @see Person
 	 */
-	public void saveOrUpdatePerson(Person p) throws DAOException;
+	public void saveOrUpdatePerson(Person p);
 	
 	/**
 	 * Suppression de la personne associé à l'identifiant id.
 	 * 
 	 * @param id L'id correspondant à la personne.
+	 * @throws DAOException Si la personne qu'on souhaite supprimer n'existe pas dans la bdd ou un pb de connexion.
 	 */
-	public void removePerson(long id);
+	public void removePerson(long id) throws DAOException;
 	
 	/**
 	 * Suppression de la personne.
 	 * 
-	 * @param p La personne à supprimer
-	 * @throws DAOException Si la personne n'existe pas.
+	 * @param p La personne à supprimer.
 	 * @see Person
 	 */
 	public void removePerson(Person p);
 	
 	/**
-	 * Suppression de toute les personnes de la base.
+	 * Suppression de toutes les personnes de la base.
+	 * 
+	 * @throws DAOException Si aucune personne n'est dans la base ou s'il y'a un pb de connexion
 	 */
-	public void removeAllPersons();
+	public void removeAllPersons() throws DAOException;
 	
 	/**
 	 * Calcul le nombre de personnes de la base.
 	 * 
 	 * @return Le nombre de personne.
-	 * @throws DAOException Si exception levé avant.
-	 * @throws JdbcToolsException Si la connection n'est pas établie.
 	 */
-	public int countPersons() throws DAOException, DatabaseManagerException;
+	public int countPersons();
 }
