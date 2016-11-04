f4102f94fa03f0cab4c9f40fa5fffe67907156a9
sidediallo <sidediallo@yahoo.fr>
2016-11-04 23:12:53 +0100

    Màj des tests

--- src/test/java/fr/univ_amu/yearbook/dao/impl/BeanToResultSetImplTest.java
+++ src/test/java/fr/univ_amu/yearbook/dao/impl/BeanToResultSetImplTest.java
@@ -31,17 +31,17 @@ public class BeanToResultSetImplTest {
 		String query = "INSERT INTO YEARBOOK_Person (lastName, firstName, email, homepage, birthDate, pwd, idG)"
 				+ "VALUES (?, ?, ?, ?, ?, PASSWORD(?), ?)";
 		
-		p1.setLastName("CAMARA");
-		p1.setFirstName("Moussa");
-		p1.setEmail("camara.moussa@localhost.fr");
-		p1.setHomePage("www.camara_moussa.fr");
+		p1.setLastName("DIALLO");
+		p1.setFirstName("Aboubacar Sidy");
+		p1.setEmail("diallo.aboubacar@localhost.fr");
+		p1.setHomePage("www.diallo_aboubacar.fr");
 		p1.setBirthDate(Date.valueOf("2016-01-01"));
-		p1.setPwd("moussa");
+		p1.setPwd("aboubacar");
 		p1.setIdG((long) 1);
 		assertEquals (1, mapper.insertOrUpdate(p1, query, parametersList));
 		
 		Person p2 = dao.findPerson(1);
-		p2.setEmail("sidediallo@yahoo.fr");
+		p2.setEmail("aboubacar_diallo@localhost.fr");
 		String[] parametersListUpdate = {"email", "id"};
 		String queryUpdate = "UPDATE YEARBOOK_Person SET email = ? WHERE id = ?";
 		int result = mapper.insertOrUpdate(p2, queryUpdate, parametersListUpdate);
