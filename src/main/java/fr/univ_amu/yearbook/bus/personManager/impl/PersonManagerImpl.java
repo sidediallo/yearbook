cb27cb153a7f06f68fa4ae89d8b4df7e9ab3e674
sidediallo <sidediallo@yahoo.fr>
2016-11-23 20:15:11 +0100

    Implémentation de Manager Person.

--- src/main/java/fr/univ_amu/yearbook/bus/personManager/impl/PersonManagerImpl.java
+++ src/main/java/fr/univ_amu/yearbook/bus/personManager/impl/PersonManagerImpl.java
@@ -2,55 +2,47 @@ package fr.univ_amu.yearbook.bus.personManager.impl;
 
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import fr.univ_amu.yearbook.bean.Person;
+import fr.univ_amu.yearbook.bus.exception.ManagerException;
 import fr.univ_amu.yearbook.bus.personManager.IPersonManager;
-import fr.univ_amu.yearbook.bus.personManager.exception.PersonManagerException;
-import fr.univ_amu.yearbook.dao.exception.DatabaseManagerException;
+import fr.univ_amu.yearbook.dao.IPersonDAO;
 import fr.univ_amu.yearbook.dao.exception.DAOException;
-import fr.univ_amu.yearbook.dao.impl.PersonDAOImpl;
 
 /**
- * <b>PersonManagerImpl</b> implémente IPersonManager.
- *
+ * <b>PersonManagerImpl</b> implémente le manager IPersonManager.
+ * 
  * @see Person
- * @see PersonManagerException
- * @see IPersonManager
+ * @see ManagerException
  *  
- * @author Aboubacar Sidy DIALLO & Inoussa ZONGO
+ * @author Aboubacar Sidy DIALLO
+ * @author Inoussa ZONGO
  * @version 1.0
  *
  */
+@Service("personManager")
 public class PersonManagerImpl implements IPersonManager {
 	
 	/**
 	 * L'objet qui établi le lien avec la PersonDao.
 	 * 
-	 * @see PersonManagerImpl#PersonManagerImpl()
-	 * @see PersonManagerImpl#init()
-	 * @see PersonManagerImpl#findPerson(long)
-	 * @see PersonManagerImpl#findAllPersons()
-	 * @see PersonManagerImpl#saveOrUpdatePerson(Person)
-	 * @see PersonManagerImpl#removePerson(long)
-	 * @see PersonManagerImpl#removePerson(Person)
-	 * @see PersonManagerImpl#removeAllPersons()
+	 * @see #findPerson(long)
+	 * @see #findAllPersons()
+	 * @see #saveOrUpdatePerson(Person)
+	 * @see #removePerson(long)
+	 * @see #removePerson(Person)
+	 * @see #removeAllPersons()
 	 */
-	private PersonDAOImpl dao;
+	@Autowired
+	private IPersonDAO dao;
 	
 	/**
 	 * Constructeur par défaut de la classe.
 	 */
 	public PersonManagerImpl() {
 		super();
-	}
-	
-	/**
-	 * Initialisation de la PersonDao
-	 * 
-	 * @throws DatabaseManagerException Si une exception est levée.
-	 */
-	public void init() throws DatabaseManagerException {
-		dao = new PersonDAOImpl();
-		//dao.init();
 	}
 
 	/**
@@ -59,48 +51,47 @@ public class PersonManagerImpl implements IPersonManager {
 	 * @param id L'id de la personne.
 	 * @return
 	 * 		La personne dont l'indentifiant est rentré en paramètre de la méthode. 
-	 * @throws PersonManagerException Si la personne rattachée à l'id n'existe pas.
+	 * @throws ManagerException Si la personne rattachée à l'id n'existe pas.
 	 */
 	@Override
-	public Person findPerson(long id) throws PersonManagerException {
+	public Person findPerson(long id) throws ManagerException {
 		try {
 			return dao.findPerson(id);
 		} catch (DAOException e) {
-			e.getMessage();
+			throw new ManagerException(e.getCause());
 		}
-		return null;
 	}
 
 	/**
 	 * Retourne la liste des personnes existantes.
 	 * 
 	 * @return
-	 * 		La liste de personnes.
+	 * 		La liste des personnes.
+	 * @throws ManagerException Si la bd est vide ou si une autre exception est levée. 
 	 */
 	@Override
-	public Collection<Person> findAllPersons() {
+	public Collection<Person> findAllPersons() throws ManagerException {
 		try {
 			return dao.findAllPersons();
 		} catch (DAOException e) {
-			e.getMessage();
+			throw new ManagerException(e.getCause());
 		}
-		return null;
 	}
 
 	/**
 	 * Création ou mise à jour d'une personne.
 	 * 
 	 * @param p La personne.
-	 * @throws PersonManagerException Si la personne qu'on souhaite rajouter existe déjà
+	 * @throws ManagerException Si la personne qu'on souhaite rajouter existe déjà
 	 * 		   ou si la personne qu'on souhaite mettre à jour n'existe pas.
 	 * @see Person
 	 */
 	@Override
-	public void saveOrUpdatePerson(Person p) throws PersonManagerException {
+	public void saveOrUpdatePerson(Person p) throws ManagerException {
 		try {
 			dao.saveOrUpdatePerson(p);
 		} catch (DAOException e) {
-			e.getMessage();
+			throw new ManagerException(e.getCause());
 		}
 	}
 
@@ -108,47 +99,76 @@ public class PersonManagerImpl implements IPersonManager {
 	 * Suppression de la personne associé à l'identifiant id.
 	 * 
 	 * @param id L'id correspondant à la personne.
-	 * @throws PersonManagerException Si la personne rattachée à l'id n'existe pas.
+	 * @throws ManagerException Si la personne rattachée à l'id n'existe pas.
 	 */
 	@Override
-	public void removePerson(long id) throws PersonManagerException {
-		dao.removePerson(id);
+	public void removePerson(long id) throws ManagerException {
+		try {
+			dao.removePerson(id);
+		} catch (DAOException e) {
+			throw new ManagerException(e.getCause());
+		}
 	}
 
 	/**
 	 * Suppression de la personne.
 	 * 
 	 * @param p La personne à supprimer
-	 * @throws PersonManagerException Si la personne n'existe pas.
+	 * @throws ManagerException Si la personne n'existe pas.
+	 * @see Person
 	 */
 	@Override
-	public void removePerson(Person p) throws PersonManagerException {
-		dao.removePerson(p);
+	public void removePerson(Person p) throws ManagerException {
+		try {
+			dao.removePerson(p);
+		} catch (DAOException e) {
+			throw new ManagerException(e.getCause());
+		}
 	}
 
 	/**
 	 * Suppression de toute les personnes de la base.
+	 * @throws ManagerException Si la bd est vide ou si une autre type d'exception est levée. 
 	 */
 	@Override
-	public void removeAllPersons() {
-		dao.removeAllPersons();
+	public void removeAllPersons() throws ManagerException {
+		try {
+			dao.removeAllPersons();
+		} catch (DAOException e) {
+			throw new ManagerException(e.getCause());
+		}
+	}
+	
+	/**
+	 * Calcul le nombre de personnes de la base.
+	 * 
+	 * @return Le nombre de personne.
+	 * @throws ManagerException Si la connexion échoue. 
+	 */
+	@Override
+	public int countPersons() throws ManagerException {
+		try {
+			return dao.countPersons();
+		} catch (DAOException e) {
+			throw new ManagerException(e.getCause());
+		}
 	}
 
 	/**
-	 * Retourne la PersonDao.
+	 * Retourne une instance de la classe qui implémente l'interface IPersonDao.
 	 * 
-	 * @return la PersonDao.
+	 * @return L'instance.
 	 */
-	public PersonDAOImpl getDao() {
+	public IPersonDAO getDao() {
 		return dao;
 	}
 
 	/**
-	 * Mise à jour de PersonDaoImpl.
+	 * Mise à jour de l'instance de la classe qui implémente l'interface IPersonDao.
 	 * 
-	 * @param dao La nouvelle PersonDao.
+	 * @param dao La nouvelle instance.
 	 */
-	public void setDao(PersonDAOImpl dao) {
+	public void setDao(IPersonDAO dao) {
 		this.dao = dao;
 	}
 }
